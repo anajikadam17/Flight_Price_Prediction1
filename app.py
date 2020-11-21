5b9f4347fb13f7a108f2f304bbb795e088c75609
anajikadam17 <anajikadam17@gmail.com>
2020-11-21 21:09:08 +0530

    commit3

--- app.py
+++ app.py
@@ -1,27 +1,47 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Mar  1 12:40:42 2020
+Created on Tue Sep 22 09:36:28 2020
 
 @author: Anaji
 """
+from flask import Flask, render_template,request
+from flask_cors import cross_origin
+
 import numpy as np
-from flask import Flask, request, jsonify, render_template
-import pickle
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+warnings.filterwarnings("ignore", category=UserWarning)
+
+
+from model_3 import FlightFarePredict
 
 app = Flask(__name__)
 
-
-@app.route('/',methods=['GET'])
+@app.route('/')
 def home():
-    return "YAY!!! its working"
+    return render_template("home.html")
 
+@app.route("/predict", methods = ["GET", "POST"])
+@cross_origin()
+def predict():
+    if request.method == "POST":
+        date_dep = request.form["Dep_Time"]
+        date_arr = request.form["Arrival_Time"]
+        Total_stops = int(request.form["stops"])
+        airline = request.form['airline']
+        Source = request.form["Source"]
+        Dest = request.form["Destination"]
+        FlightFarePredictObj = FlightFarePredict()
+        my_prediction = FlightFarePredictObj.predictFlightFare(date_dep, 
+                                            date_arr, Total_stops, airline,
+                                            Source, Dest)
+        output=round(my_prediction[0],2)
+        return render_template('home.html',prediction_text="Your Flight price is Rs. {}".format(output))
 
-@app.route('/add',methods=['GET'])
-def addNum():
-    a = 2
-    b = 3
-    return "The sm of {} and {} is {}".format(a,b,a+b)
-
+    return render_template("home.html")
+@app.route('/index')
+def index():
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
